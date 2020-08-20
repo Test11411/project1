f712887fa4597382bf4740c663afbe04cde5ff1a
Suman11411 <49188734+Suman11411@users.noreply.github.com>
2020-08-20 19:01:54 +0530

    Create Hello java file

---
+++ Hello.java
@@ -0,0 +1,14 @@
+//Java program to swap two numbers.
+import java.util.*;
+
+class SwapTwoNumbers
+{
+	public static void main(String []args)
+	{
+		int a,b;
+		for(a=1,a<=10,a++)
+    {
+		System.out.println("Hello Raghu....");
+    }
+	}
+}
